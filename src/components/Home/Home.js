9067a0f50779c0ecf8785be037ceab4180f016ae
Mohammad Mahbubul Alam <alam.diu13@gmail.com>
2022-06-08 19:50:45 +0600

    changed resume link

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -50,7 +50,7 @@ const Home = () => {
 
                             {/* <a role="button" class="btn-color-one text-decoration-none" href="https://drive.google.com/file/d/1-12ff33sHzS1B-8eGrRUwSo1om6ICY7Z/view?usp=sharing" target="_blank">Download Resume</a> */}
 
-                            <a role="button" class="btn-color-one text-decoration-none" href="https://drive.google.com/file/d/1hi3rysyfovpc3unQ2rB6QPjbqiyZGBsS/view?usp=sharing" target="_blank">Download Resume</a>
+                            <a role="button" class="btn-color-one text-decoration-none" href="https://drive.google.com/file/d/1bJ5pJ7JwzSWeoIOvuV3LZgEQnHWRJUAf/view?usp=sharing" target="_blank">Download Resume</a>
 
                             <a role="button" class="btn-color-two text-decoration-none mx-2" href="#resume">Online CV</a>
                         </div>   
